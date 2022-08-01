23d49585e4b1902a3c223264678b675e99091818
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-08-01 02:00:47 +0200

    Dummy Text in AboutView.vue & added Scrollbar styles 🚀

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,44 @@
 <script setup></script>
 
 <template>
-  <div class="about text-center font-mono text-2xl mt-10">
-    <h1>This is an about page</h1>
+  <div class="about container mx-auto px-4 text-2xl mt-10">
+    <h1 class="text-4xl mb-5 text-center font-mono">This is an about page</h1>
+    <p class="mb-3">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad atque beatae
+      consequuntur cumque doloremque dolores doloribus exercitationem explicabo
+      harum laborum minima molestias, nam necessitatibus nisi non obcaecati quas
+      repellat repellendus saepe velit veritatis vero voluptas voluptatibus.
+      Assumenda distinctio dolore libero maiores maxime molestias nesciunt quam
+      quas rerum vel, velit voluptas?
+    </p>
+    <p class="mb-3">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis,
+      totam voluptates. Adipisci architecto asperiores aspernatur cumque dolor
+      eum eveniet impedit inventore ipsum itaque necessitatibus non nostrum
+      nulla numquam odit officia pariatur porro possimus quam quasi qui, quo,
+      ratione, recusandae reprehenderit similique sunt totam voluptas. Itaque
+      iusto magni modi quas? Inventore iusto nam officiis quibusdam quod!
+      Accusamus autem dolor explicabo fuga ut! Alias aliquid aut deserunt
+      excepturi hic iure numquam perferendis quibusdam reiciendis reprehenderit.
+      Architecto commodi dignissimos perspiciatis? Aliquid rem, veritatis.
+    </p>
+    <p class="mb-3">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet architecto
+      autem consequatur cumque dolorum eius eos labore magnam, minus non nulla
+      quod soluta tempore! Dignissimos distinctio eveniet nesciunt quos tenetur,
+      voluptas voluptates. Accusamus accusantium ad amet aspernatur consectetur
+      cupiditate delectus deleniti dolorem, doloremque ex harum illo maxime
+      minus neque nisi odio pariatur quia, quis reiciendis repudiandae ullam
+      unde voluptatum. Dignissimos, doloremque eius eligendi excepturi
+      exercitationem illo impedit ipsam nam non omnis quod rem repudiandae
+      voluptas. Assumenda atque aut consequuntur cum deserunt dolor dolorem, est
+      illum perferendis quia quod quos, rem sapiente tenetur veniam! Animi
+      facilis hic minima sequi tenetur veritatis?
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. A aliquid autem,
+      dolorem earum excepturi fugit itaque nemo officia optio porro quo sint vel
+      vitae voluptatem voluptates. Atque eaque natus voluptates!
+    </p>
   </div>
 </template>
